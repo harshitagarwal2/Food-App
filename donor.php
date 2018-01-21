4cfc89586457ada1a28ad618aa6fce5f9b712420
harsh <harshit_agrawal36@yahoo.com>
2018-01-21 07:40:28 -0800

    donor

--- donor.php
+++ donor.php
@@ -5,6 +5,7 @@ require_once('donor_functions.php');
 
 session_start();
 
+$donorid = $description = $foodPic = $location= "";
 // If session variable is not set it will redirect to login page
 if(isset($_SESSION['username']) || !empty($_SESSION['username']))
 {
@@ -13,6 +14,9 @@ $username = $_SESSION['username'];
 $password = $_SESSION['password'] ;
 $connection = createConnection($GLOBALS['hostname'],$GLOBALS['database'], $GLOBALS['username_db'],$GLOBALS['password_db']);
 validateSession($connection,$username,$password);
+$text = getDonorID($connection,$username,$password);
+$a = explode(',' , $text);
+$donorid = $a[0];
 $connection->close();
 }
 else{
@@ -20,6 +24,43 @@ header("location: login.php");
 exit;
 };
 
+// Store the information into variable $description
+if(isset($_POST['foodDesc'])){
+  $description = fix_string($_POST["foodDesc"]);
+}
+
+if(isset($_POST['foodpic'])){
+ $foodPic = fix_string($_POST["foodpic"]);
+}
+
+if(isset($_POST['location'])){
+ $location = fix_string($_POST["location"]);
+}
+
+function fix_string($string) {
+  if (get_magic_quotes_gpc()) $string = stripslashes($string);
+   return htmlentities ($string);
+ }
+
+if(isset($_POST['submit'])){
+  global $description,$foodPic,$location ;
+  $connection= createConnection($GLOBALS['hostname'],$GLOBALS['database'], $GLOBALS['username_db'],$GLOBALS['password_db']);
+  insertData($GLOBALS['description'],$GLOBALS['foodPic'],$connection,$GLOBALS['donorid'], $GLOBALS['location']);
+}
+
+function insertData($description,$foodPic,$conn,$donorid,$location){
+  $donorid= mysql_entities_fix_string($conn,$donorid);
+  $description= mysql_entities_fix_string($conn, $description);
+  $foodPic = mysql_entities_fix_string($conn,$foodPic);
+  $location = mysql_entities_fix_string($conn,$location);
+  $sql = "INSERT INTO foodlist (donorid, description, foodPic, location)
+    VALUES ('$donorid', '$description', '$foodPic', '$location')";
+    $result = $conn->query($sql);
+    if (!$result) die ("Database access failed: " . $conn->error);
+      else{
+        echo "saved";
+      }
+}
 
 ?>
 
@@ -55,13 +96,13 @@ exit;
               <div class="col">
                 <div class="card-body">
                   <h1 class="mb-3 text-center">Food Posting</h1>
-                    <form class="mx-auto" method="POST" action="" enctype="multipart/form-data">
-                      <div class="form-group">
+  <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" onsubmit="return validate(this)" >
+                        <div class="form-group">
                         <input type="text" class="form-control" name="foodDesc" placeholder="Food Description">
                       </div>
                       <div class="form-group text-left">
                         <label for="picture">Upload a photo of the food</label>
-                        <input type="file" class="form-control-file" name="foodPic">
+                        <input type="file" class="form-control-file" name="foodpic">
                       </div>
                       <button type="submit" name="submit" class="btn btn-primary">Submit</button>
                     </form>
--- donor_functions.php
+++ donor_functions.php
@@ -18,4 +18,18 @@ function validateSession($connection,$username,$password){
 }
 
 
+function getDonorID($connection,$username,$password){
+  $sql_username =  mysql_entities_fix_string($connection, $username);
+  $sql_password =  mysql_entities_fix_string($connection, $password);
+  $sql = "SELECT * FROM donor WHERE username = '$sql_username'";
+  $result = $connection->query($sql);
+   if (!$result) die($connection->error);
+     elseif ($result->num_rows) {
+          $row = $result->fetch_array(MYSQLI_NUM);
+           $text = implode(",", $row);
+           return $text;
+          $result->close();
+}
+}
+
 ?>
